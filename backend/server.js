ab7ccdcb18e77d2623049b26e9fc2cd929c9a5e9
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2024-06-27 20:17:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 const port = 5000;
 
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://beatloop-project.onrender.com/', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
